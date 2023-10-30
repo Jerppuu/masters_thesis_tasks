7bb6e14f01648705b9fc1ba793764f96402e62e0
Jere Pesälä <j.pesala@protonmail.com>
2023-10-30 11:00:44 +0200

    add read json file implementation

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,20 @@
+#include <fstream>
 #include <iostream>
+#include "nlohmann/json.hpp"
 
 int main() {
-  std::cout << "Hello World!" << std::endl;
+  std::ifstream istrm("src/example.json");
+  
+  if (!istrm.is_open())
+    std::cout << "failed to open example.json" << '\n';
+  else
+  {
+      nlohmann::json data = nlohmann::json::parse(istrm);
+      std::cout << "Json file successfully read. Printing json key-value pairs..." << std::endl;
+      
+      for (auto& item : data.items()){
+        std::cout << item.key() << " : " << item.value() << std::endl;
+      }
+  }
   return 0;
 } 
